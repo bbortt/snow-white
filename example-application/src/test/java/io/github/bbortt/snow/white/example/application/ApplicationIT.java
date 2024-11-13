e10274517ea1b45f1bc68c78c34e24c3e5827f9e
Timon Borter <timon.borter@gmx.ch>
2024-11-13 22:29:41 +0100

    chore: initial data pipeline

---
+++ example-application/src/test/java/io/github/bbortt/snow/white/example/application/ApplicationIT.java
@@ -0,0 +1,12 @@
+package io.github.bbortt.snow.white.example.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+}
