0ec32cfbf3ee28c5e1bfcbf32ea716d5cac0cca6
Timon Borter <timon.borter@gmx.ch>
2025-02-25 16:50:05 +0100

    chore: switch to more meaningful ping-pong api

---
+++ example-application/src/main/java/io/github/bbortt/snow/white/example/application/api/PingApiImpl.java
@@ -0,0 +1,14 @@
+package io.github.bbortt.snow.white.example.application.api;
+
+import io.github.bbortt.snow.white.example.application.model.PongResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingApiImpl implements PingApi {
+
+  @Override
+  public ResponseEntity<PongResponse> getPing(String message) {
+    return ResponseEntity.ok(PongResponse.builder().message(message).build());
+  }
+}
