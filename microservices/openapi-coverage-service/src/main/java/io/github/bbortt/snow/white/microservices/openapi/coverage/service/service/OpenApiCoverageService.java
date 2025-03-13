e7a23af3d9a13af5d873d2c832f9fb7c395ca3f5
Timon Borter <timon.borter@gmx.ch>
2025-03-13 07:46:20 +0100

    feat(openapi-coverage-service): build with docker

--- microservices/openapi-coverage-service/src/main/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/service/OpenApiCoverageService.java
+++ microservices/openapi-coverage-service/src/main/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/service/OpenApiCoverageService.java
@@ -1,5 +1,7 @@
 package io.github.bbortt.snow.white.microservices.openapi.coverage.service.service;
 
+import static io.github.bbortt.snow.white.microservices.openapi.coverage.service.service.AttributeFilter.attributeFilters;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -7,9 +9,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class TelemetryAnalysisService {
+public class OpenApiCoverageService {
 
   private final OpenApiService openApiService;
+  private final OpenTelemetryService openTelemetryService;
 
   public void calculateTelemetry(
     String otelServiceName,
@@ -17,12 +20,18 @@ public class TelemetryAnalysisService {
     String apiVersion
   ) {
     try {
-      openApiService.findAndParseOpenApi(
+      var openApi = openApiService.findAndParseOpenApi(
         new OpenApiService.OpenApiIdentifier(
           otelServiceName,
           apiName,
           apiVersion
         )
+      );
+
+      var openTelemetryData = openTelemetryService.findTracingData(
+        "example-application",
+        "1d",
+        attributeFilters().build()
       );
     } catch (OpenApiNotIndexedException | UnparseableOpenApiException e) {
       throw new IllegalArgumentException(e);
---
+++ microservices/openapi-coverage-service/src/main/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/service/OpenTelemetryService.java
@@ -0,0 +1,80 @@
+package io.github.bbortt.snow.white.microservices.openapi.coverage.service.service;
+
+import static io.github.bbortt.snow.white.microservices.openapi.coverage.service.service.OpenTelemetryData.parseOpenTelemetryData;
+import static io.opentelemetry.javaagent.shaded.io.opentelemetry.semconv.ServiceAttributes.SERVICE_NAME;
+import static org.springframework.util.CollectionUtils.isEmpty;
+
+import com.influxdb.client.InfluxDBClient;
+import io.github.bbortt.snow.white.microservices.openapi.coverage.service.config.InfluxDBProperties;
+import jakarta.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OpenTelemetryService {
+
+  private final InfluxDBClient influxDBClient;
+  private final InfluxDBProperties influxDBProperties;
+
+  public List<OpenTelemetryData> findTracingData(
+    String serviceName,
+    String lookbackRange,
+    @Nullable List<AttributeFilter> attributeFilters
+  ) {
+    var fluxBuilder = new StringBuilder();
+    fluxBuilder
+      .append(buildBucketFilter())
+      .append(buildRangeFilter(lookbackRange))
+      .append(" |> filter(fn: (r) => r._measurement == \"spans\") ")
+      .append(buildServiceNameFilter(serviceName))
+      .append(" |> filter(fn: (r) => r._field == \"attributes\") ");
+
+    if (!isEmpty(attributeFilters)) {
+      attributeFilters.forEach(attributeFilter ->
+        fluxBuilder.append(attributeFilter.toFluxString())
+      );
+    }
+
+    fluxBuilder.append(
+      " |> keep(columns: [\"_field\", \"_value\", \"span_id\", \"trace_id\"]) "
+    );
+
+    var fluxQuery = fluxBuilder.toString();
+    logger.trace("Firing flux query: {}", fluxQuery);
+
+    var fluxTables = influxDBClient.getQueryApi().query(fluxQuery);
+
+    var openTelemetryData = new ArrayList<OpenTelemetryData>();
+    fluxTables.forEach(fluxTable ->
+      fluxTable
+        .getRecords()
+        .forEach(fluxRecord ->
+          openTelemetryData.add(parseOpenTelemetryData(fluxRecord))
+        )
+    );
+    return openTelemetryData;
+  }
+
+  private String buildBucketFilter() {
+    return "from(bucket: \"" + influxDBProperties.getBucket() + "\") ";
+  }
+
+  private String buildRangeFilter(String lookbackRange) {
+    return "|> range(start: -" + lookbackRange + ") ";
+  }
+
+  private String buildServiceNameFilter(String serviceName) {
+    return (
+      "|> filter(fn: (r) => r[\"" +
+      SERVICE_NAME.getKey() +
+      "\"] == \"" +
+      serviceName +
+      "\") "
+    );
+  }
+}
