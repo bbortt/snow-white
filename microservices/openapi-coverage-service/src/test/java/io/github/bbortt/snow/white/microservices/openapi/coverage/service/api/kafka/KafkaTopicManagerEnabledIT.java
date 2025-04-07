316e1d923ec1421d6a18ce5b8640a14832c860ff
Timon Borter <timon.borter@gmx.ch>
2025-04-07 19:59:58 +0200

    ci(openapi-coverage-service): fix kafka topic manager test

--- microservices/openapi-coverage-service/src/test/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/api/kafka/KafkaTopicManagerEnabledIT.java
+++ microservices/openapi-coverage-service/src/test/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/api/kafka/KafkaTopicManagerEnabledIT.java
@@ -1,5 +1,6 @@
 package io.github.bbortt.snow.white.microservices.openapi.coverage.service.api.kafka;
 
+import static io.github.bbortt.snow.white.microservices.openapi.coverage.service.AbstractOpenApiCoverageServiceIT.ADMIN_TOKEN;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.github.bbortt.snow.white.microservices.openapi.coverage.service.AbstractOpenApiCoverageServiceIT;
@@ -12,7 +13,14 @@ import org.springframework.test.annotation.DirtiesContext;
 @Isolated
 @DirtiesContext
 @SpringBootTest(
-  properties = "io.github.bbortt.snow.white.microservices.openapi.coverage.service.init-topics=true"
+  properties = {
+    "influxdb.org=snow-white",
+    "influxdb.bucket=raw-data",
+    "influxdb.token=" + ADMIN_TOKEN,
+    "io.github.bbortt.snow.white.microservices.openapi.coverage.service.init-topics=true",
+    "io.github.bbortt.snow.white.microservices.openapi.coverage.service.calculation-request-topic=snow-white-coverage-request",
+    "io.github.bbortt.snow.white.microservices.openapi.coverage.service.openapi-calculation-response-topic=snow-white-openapi-calculation-response",
+  }
 )
 class KafkaTopicManagerEnabledIT extends AbstractOpenApiCoverageServiceIT {
 
