f9bf1011c9373eddae8958b49d1a781f449cbad4
Timon Borter <timon.borter@gmx.ch>
2025-04-21 17:20:20 +0200

    fix(api-gateway): routing criteria resource to microservice

--- microservices/openapi-coverage-service/src/test/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/config/InfluxDBPropertiesTest.java
+++ microservices/openapi-coverage-service/src/test/java/io/github/bbortt/snow/white/microservices/openapi/coverage/service/config/InfluxDBPropertiesTest.java
@@ -7,9 +7,14 @@
 package io.github.bbortt.snow.white.microservices.openapi.coverage.service.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.InitializingBean;
 
 class InfluxDBPropertiesTest {
@@ -24,5 +29,71 @@ class InfluxDBPropertiesTest {
   @Test
   void isInitializingBean() {
     assertThat(fixture).isInstanceOf(InitializingBean.class);
+  }
+
+  @Nested
+  class AfterPropertiesSet {
+
+    public static Stream<String> emptyOrNullString() {
+      return Stream.of(null, "", " ");
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyOrNullString")
+    void shouldThrow_whenUrlIsEmpty(String url) {
+      fixture.setUrl(url);
+
+      fixture.setToken("token");
+      fixture.setOrg("org");
+      fixture.setBucket("bucket");
+
+      assertThatConnectionExceptionIsBeingThrown();
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyOrNullString")
+    void shouldThrow_whenTokenIsEmpty(String token) {
+      fixture.setUrl("url");
+
+      fixture.setToken(token);
+
+      fixture.setOrg("org");
+      fixture.setBucket("bucket");
+
+      assertThatConnectionExceptionIsBeingThrown();
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyOrNullString")
+    void shouldThrow_whenOrgIsEmpty(String org) {
+      fixture.setUrl("url");
+      fixture.setToken("token");
+
+      fixture.setOrg(org);
+
+      fixture.setBucket("bucket");
+
+      assertThatConnectionExceptionIsBeingThrown();
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyOrNullString")
+    void shouldThrow_whenBucketIsEmpty(String bucket) {
+      fixture.setUrl("url");
+      fixture.setToken("token");
+      fixture.setOrg("org");
+
+      fixture.setBucket(bucket);
+
+      assertThatConnectionExceptionIsBeingThrown();
+    }
+
+    private void assertThatConnectionExceptionIsBeingThrown() {
+      assertThatThrownBy(() -> fixture.afterPropertiesSet())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage(
+          "InfluxDB connection not properly configured! Please read the docs."
+        );
+    }
   }
 }
