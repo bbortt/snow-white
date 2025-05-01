6c27cd4c7ec9bd834ae20d8758baacc31bde3d7c
Timon Borter <timon.borter@gmx.ch>
2025-05-01 12:11:58 +0200

    fix(api-gateway): get rid of authentication

--- microservices/api-gateway/src/main/webapp/app/app.tsx
+++ microservices/api-gateway/src/main/webapp/app/app.tsx
@@ -8,13 +8,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
-import { getSession } from 'app/shared/reducers/authentication';
 import { getProfile } from 'app/shared/reducers/application-profile';
 import Header from 'app/shared/layout/header/header';
 import Footer from 'app/shared/layout/footer/footer';
-import { hasAnyAuthority } from 'app/shared/auth/private-route';
 import ErrorBoundary from 'app/shared/error/error-boundary';
-import { AUTHORITIES } from 'app/config/constants';
 import AppRoutes from 'app/routes';
 
 const baseHref = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
@@ -23,13 +20,10 @@ export const App = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getSession());
     dispatch(getProfile());
   }, []);
 
   const currentLocale = useAppSelector(state => state.locale.currentLocale);
-  const isAuthenticated = useAppSelector(state => state.authentication.isAuthenticated);
-  const isAdmin = useAppSelector(state => hasAnyAuthority(state.authentication.account.authorities, [AUTHORITIES.ADMIN]));
   const ribbonEnv = useAppSelector(state => state.applicationProfile.ribbonEnv);
   const isInProduction = useAppSelector(state => state.applicationProfile.inProduction);
   const isOpenAPIEnabled = useAppSelector(state => state.applicationProfile.isOpenAPIEnabled);
@@ -40,14 +34,7 @@ export const App = () => {
       <div className="app-container" style={{ paddingTop }}>
         <ToastContainer position="top-left" className="toastify-container" toastClassName="toastify-toast" />
         <ErrorBoundary>
-          <Header
-            isAuthenticated={isAuthenticated}
-            isAdmin={isAdmin}
-            currentLocale={currentLocale}
-            ribbonEnv={ribbonEnv}
-            isInProduction={isInProduction}
-            isOpenAPIEnabled={isOpenAPIEnabled}
-          />
+          <Header currentLocale={currentLocale} ribbonEnv={ribbonEnv} isInProduction={isInProduction} isOpenAPIEnabled={isOpenAPIEnabled} />
         </ErrorBoundary>
         <div className="container-fluid view-container" id="app-view-container">
           <Card className="jh-card">
--- microservices/api-gateway/src/main/webapp/app/shared/layout/menus/index.ts
+++ microservices/api-gateway/src/main/webapp/app/shared/layout/menus/index.ts
@@ -1,3 +1,2 @@
-export * from './admin';
 export * from './locale';
 export * from './entities';
