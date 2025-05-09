006320892811159bf121bd30a4a17c6dc5b8fdca
Timon Borter <timon.borter@gmx.ch>
2025-05-09 15:04:34 +0200

    feat(api-gateway): landing page

--- microservices/api-gateway/src/main/webapp/app/entities/menu.tsx
+++ microservices/api-gateway/src/main/webapp/app/entities/menu.tsx
@@ -13,11 +13,14 @@ const EntitiesMenu = () => {
   return (
     <>
       {/* prettier-ignore */}
-      <MenuItem icon="asterisk" to="/open-api-criterion">
-        <Translate contentKey="global.menu.entities.openApiCriterion" />
-      </MenuItem>
+      <MenuItem icon="asterisk" to="/quality-gate">
+            <Translate contentKey="global.menu.entities.qualityGate" />
+        </MenuItem>
       <MenuItem icon="asterisk" to="/quality-gate-config">
         <Translate contentKey="global.menu.entities.qualityGateConfig" />
+      </MenuItem>
+      <MenuItem icon="asterisk" to="/open-api-criterion">
+        <Translate contentKey="global.menu.entities.openApiCriterion" />
       </MenuItem>
       {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
     </>
---
+++ microservices/api-gateway/src/main/webapp/app/entities/quality-gate/quality-gate.reducer.ts
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2025 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
+import { AxiosResponse } from 'axios';
+import { createAsyncThunk, isFulfilled, isPending } from '@reduxjs/toolkit';
+import { createEntitySlice, EntityState, IQueryParams, serializeAxiosError } from 'app/shared/reducers/reducer.utils';
+import { defaultValue, IQualityGate } from 'app/shared/model/quality-gate.model';
+import { reportApi } from 'app/entities/quality-gate/report-api';
+import { QualityGateReport } from 'app/clients/report-api';
+import dayjs from 'dayjs';
+
+const initialState: EntityState<IQualityGate> = {
+  loading: false,
+  errorMessage: null,
+  entities: [],
+  entity: defaultValue,
+  updating: false,
+  totalItems: 0,
+  updateSuccess: false,
+};
+
+const fromDto = ({
+  calculationId,
+  qualityGateConfigName,
+  status,
+  calculationRequest,
+  initiatedAt,
+  openApiTestResults,
+}: QualityGateReport): IQualityGate => ({
+  calculationId,
+  qualityGateConfig: { name: qualityGateConfigName },
+  status,
+  calculationRequest: { ...calculationRequest, attributeFilters: joinAttributeFilters(calculationRequest.attributeFilters) },
+  createdAt: dayjs(initiatedAt),
+  openApiTestResults: openApiTestResults?.map(openApiTestResult => ({
+    ...openApiTestResult,
+    openApiCriterionName: openApiTestResult.id,
+  })),
+});
+
+export const joinAttributeFilters = (attributeFilters?: { [key: string]: string }): string => {
+  if (!attributeFilters || Object.keys(attributeFilters).length === 0) {
+    return '';
+  }
+
+  return Object.entries(attributeFilters)
+    .map(([key, value]) => `${key}=${value}`)
+    .join(', ');
+};
+
+// Actions
+
+export const getEntities = createAsyncThunk(
+  'qualityGate/fetch_entity_list',
+  async ({ page, size, sort }: IQueryParams): Promise<AxiosResponse<IQualityGate[]>> => {
+    return reportApi.listQualityGateReports(page, size, sort).then(response => ({
+      ...response,
+      data: response.data.map(qualityGateReport => fromDto(qualityGateReport)),
+    }));
+  },
+  { serializeError: serializeAxiosError },
+);
+
+export const getEntity = createAsyncThunk(
+  'qualityGate/fetch_entity',
+  async (calculationId: string): Promise<AxiosResponse<IQualityGate>> => {
+    return reportApi.getReportByCalculationId(calculationId).then(response => ({
+      ...response,
+      data: fromDto(response.data),
+    }));
+  },
+  { serializeError: serializeAxiosError },
+);
+
+// slice
+
+export const QualityGateSlice = createEntitySlice({
+  name: 'qualityGate',
+  initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(getEntity.fulfilled, (state, action) => {
+        state.loading = false;
+        state.entity = action.payload.data;
+      })
+      .addMatcher(isFulfilled(getEntities), (state, action) => {
+        const { data, headers } = action.payload;
+
+        return {
+          ...state,
+          loading: false,
+          entities: data,
+          totalItems: parseInt(headers['x-total-count'], 10),
+        };
+      })
+      .addMatcher(isPending(getEntities, getEntity), state => {
+        state.errorMessage = null;
+        state.updateSuccess = false;
+        state.loading = true;
+      });
+  },
+});
+
+export const { reset } = QualityGateSlice.actions;
+
+// Reducer
+export default QualityGateSlice.reducer;
