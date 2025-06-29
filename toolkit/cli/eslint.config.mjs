a8f6ccd494b6d86d96e273621e96daf502e513e9
Timon Borter <timon.borter@gmx.ch>
2025-06-29 16:22:50 +0200

    refactor: updated copyright headers

--- toolkit/cli/eslint.config.mjs
+++ toolkit/cli/eslint.config.mjs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
 // @ts-check
 
 // Based on: https://github.com/typescript-eslint/typescript-eslint/blob/main/eslint.config.mjs
