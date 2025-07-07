baa7385af202f0083888d909024af2dd440446fa
Timon Borter <timon.borter@gmx.ch>
2025-07-07 21:41:40 +0200

    fix(kafka-event-filter): native runtime hints for otel classes

--- microservices/kafka-event-filter/src/main/java/io/github/bbortt/snow/white/microservices/kafka/event/filter/config/NativeRuntimeHintsConfiguration.java
+++ microservices/kafka-event-filter/src/main/java/io/github/bbortt/snow/white/microservices/kafka/event/filter/config/NativeRuntimeHintsConfiguration.java
@@ -6,14 +6,28 @@
 
 package io.github.bbortt.snow.white.microservices.kafka.event.filter.config;
 
+import static java.util.Collections.addAll;
+import static org.springframework.aot.hint.MemberCategory.INVOKE_PUBLIC_METHODS;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 
+@Slf4j
 @Configuration
 @ImportRuntimeHints(
-  NativeRuntimeHintsConfiguration.ConfluentKafkaRuntimeHints.class
+  {
+    NativeRuntimeHintsConfiguration.ConfluentKafkaRuntimeHints.class,
+    NativeRuntimeHintsConfiguration.OtelRuntimeHints.class,
+  }
 )
 public class NativeRuntimeHintsConfiguration {
 
@@ -62,6 +76,49 @@ public class NativeRuntimeHintsConfiguration {
       } catch (ClassNotFoundException e) {
         // These classes might not be available in all versions
       }
+    }
+  }
+
+  static class OtelRuntimeHints implements RuntimeHintsRegistrar {
+
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+      scanPackageForClassesRecursively("com.google.protobuf").forEach(clazz ->
+        hints.reflection().registerType(clazz, INVOKE_PUBLIC_METHODS)
+      );
+      scanPackageForClassesRecursively("io.opentelemetry.proto").forEach(
+        clazz -> hints.reflection().registerType(clazz, INVOKE_PUBLIC_METHODS)
+      );
+    }
+
+    @VisibleForTesting
+    static Set<Class<?>> scanPackageForClassesRecursively(String packageName) {
+      var scanner = new ClassPathScanningCandidateComponentProvider(false);
+
+      scanner.addIncludeFilter(
+        new RegexPatternTypeFilter(Pattern.compile(".*"))
+      );
+
+      Set<Class<?>> classes = new HashSet<>();
+
+      scanner
+        .findCandidateComponents(packageName)
+        .forEach(beanDefinition -> {
+          try {
+            Class<?> clazz = Class.forName(beanDefinition.getBeanClassName());
+            classes.add(clazz);
+
+            // Add nested classes
+            Class<?>[] nestedClasses = clazz.getDeclaredClasses();
+            addAll(classes, nestedClasses);
+          } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+          }
+        });
+
+      logger.debug("Scanned classes: {}", classes);
+
+      return classes;
     }
   }
 }
