fc9aa9049c9be9336ddde12cc757e04820fd248a
Timon Borter <timon.borter@gmx.ch>
2025-07-23 22:15:33 +0200

    fix(report-coordination-service): native compilation

--- microservices/kafka-event-filter/src/test/java/io/github/bbortt/snow/white/microservices/kafka/event/filter/config/NativeRuntimeHintsConfigurationTest.java
+++ microservices/kafka-event-filter/src/test/java/io/github/bbortt/snow/white/microservices/kafka/event/filter/config/NativeRuntimeHintsConfigurationTest.java
@@ -8,11 +8,92 @@ package io.github.bbortt.snow.white.microservices.kafka.event.filter.config;
 
 import static io.github.bbortt.snow.white.microservices.kafka.event.filter.config.NativeRuntimeHintsConfiguration.OtelRuntimeHints.scanPackageForClassesRecursively;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.aot.hint.MemberCategory.INVOKE_PUBLIC_METHODS;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.aot.hint.ReflectionHints;
+import org.springframework.aot.hint.RuntimeHints;
 
+@ExtendWith({ MockitoExtension.class })
 class NativeRuntimeHintsConfigurationTest {
+
+  @Mock
+  private RuntimeHints runtimeHintsMock;
+
+  @Mock
+  private ReflectionHints reflectionHintsMock;
+
+  @Mock
+  private ClassLoader classLoaderMock;
+
+  private NativeRuntimeHintsConfiguration.ConfluentKafkaRuntimeHints confluentKafkaRuntimeHints;
+  private NativeRuntimeHintsConfiguration.OtelRuntimeHints otelRuntimeHints;
+
+  @BeforeEach
+  void beforeEachSetup() {
+    confluentKafkaRuntimeHints =
+      new NativeRuntimeHintsConfiguration.ConfluentKafkaRuntimeHints();
+    otelRuntimeHints = new NativeRuntimeHintsConfiguration.OtelRuntimeHints();
+  }
+
+  @Test
+  void shouldRegisterConfluentKafkaRuntimeHints() {
+    doReturn(reflectionHintsMock).when(runtimeHintsMock).reflection();
+    doReturn(reflectionHintsMock)
+      .when(reflectionHintsMock)
+      .registerType(any(Class.class));
+
+    confluentKafkaRuntimeHints.registerHints(runtimeHintsMock, classLoaderMock);
+
+    verify(reflectionHintsMock).registerType(
+      io.confluent.kafka.serializers.context.NullContextNameStrategy.class
+    );
+    verify(reflectionHintsMock).registerType(
+      io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer.class
+    );
+    verify(reflectionHintsMock).registerType(
+      io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer.class
+    );
+    verify(reflectionHintsMock).registerType(
+      io.confluent.kafka.streams.serdes.protobuf.KafkaProtobufSerde.class
+    );
+
+    verify(reflectionHintsMock).registerType(
+      io.confluent.kafka.serializers.subject.TopicNameStrategy.class
+    );
+    verify(reflectionHintsMock).registerType(
+      io.confluent.kafka.serializers.subject.TopicRecordNameStrategy.class
+    );
+
+    verifyNoInteractions(classLoaderMock);
+  }
+
+  @Test
+  void shouldRegisterOtelKafkaRuntimeHints() {
+    doReturn(reflectionHintsMock).when(runtimeHintsMock).reflection();
+    doReturn(reflectionHintsMock)
+      .when(reflectionHintsMock)
+      .registerType(any(Class.class), eq(INVOKE_PUBLIC_METHODS));
+
+    otelRuntimeHints.registerHints(runtimeHintsMock, classLoaderMock);
+
+    verify(reflectionHintsMock, times(698)).registerType(
+      any(Class.class),
+      eq(INVOKE_PUBLIC_METHODS)
+    );
+    verifyNoInteractions(classLoaderMock);
+  }
 
   @Nested
   class ScanPackageForClassesRecursively {
---
+++ microservices/report-coordination-service/src/main/java/io/github/bbortt/snow/white/microservices/report/coordination/service/config/NativeRuntimeHintsConfiguration.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
+package io.github.bbortt.snow.white.microservices.report.coordination.service.config;
+
+import io.github.bbortt.snow.white.commons.event.OpenApiCoverageResponseEvent;
+import io.github.bbortt.snow.white.commons.event.QualityGateCalculationRequestEvent;
+import io.github.bbortt.snow.white.commons.event.dto.OpenApiTestResult;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.api.client.qualitygateapi.dto.Error;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.api.client.qualitygateapi.dto.OpenApiCriterion;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.api.client.qualitygateapi.dto.QualityGateConfig;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.junit.Failure;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.junit.TestCase;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.junit.TestSuite;
+import io.github.bbortt.snow.white.microservices.report.coordination.service.junit.TestSuites;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RegisterReflectionForBinding(
+  {
+    // For Quality-Gate API Requests
+    Error.class,
+    OpenApiCriterion.class,
+    QualityGateConfig.class,
+    // For Coverage Requests
+    QualityGateCalculationRequestEvent.class,
+    // For Coverage Responses
+    OpenApiCoverageResponseEvent.class,
+    OpenApiTestResult.class,
+    // For JUnit Report Generation
+    Failure.class,
+    TestCase.class,
+    TestSuite.class,
+    TestSuites.class,
+  }
+)
+public class NativeRuntimeHintsConfiguration {}
