89ebdfbc7d27116b52f596c02c1e4688b1eca2c3
Timon Borter <timon.borter@gmx.ch>
2025-08-13 18:32:17 +0200

    chore(cli): include api spec pattern in config

--- toolkit/cli/src/actions/calculate.spec.ts
+++ toolkit/cli/src/actions/calculate.spec.ts
@@ -11,11 +11,8 @@ import { AxiosError } from 'axios';
 import { afterAll, beforeEach, describe, expect, it, jest, mock, spyOn } from 'bun:test';
 
 import type { QualityGateApi } from '../clients/quality-gate-api';
+import { QUALITY_GATE_CALCULATION_FAILED } from '../common/exit-codes';
 import { calculate } from './calculate';
-import type { CalculateOptions } from './calculate.options';
-import { INVALID_CONFIG_FORMAT, QUALITY_GATE_CALCULATION_FAILED } from '../common/exit-codes';
-import { resolveSnowWhiteConfig } from '../common/config';
-import { GlobalOptions } from './global.options';
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 mock.module('node:process', () => ({
@@ -143,116 +140,6 @@ describe('calculate', () => {
       await calculate(getQualityGateApi(qualityGateApiMock), optionsWithoutUrl);
 
       expect(mockConsoleLog).toHaveBeenNthCalledWith(6, expect.stringContaining('Base URL: undefined'));
-    });
-  });
-
-  describe('configuration sanitation', () => {
-    const expectCombinationError = (options: CalculateOptions): void => {
-      expect(calculate(getQualityGateApi(qualityGateApiMock), options)).resolves.toBeUndefined();
-
-      expect(mockConsoleError).toHaveBeenNthCalledWith(
-        1,
-        expect.stringContaining('❌ You cannot use a config file in combination with these calculation parameters:'),
-      );
-      expect(mockConsoleError).toHaveBeenNthCalledWith(2, expect.stringContaining('\t- qualityGate'));
-      expect(mockConsoleError).toHaveBeenNthCalledWith(3, expect.stringContaining('\t- serviceName'));
-      expect(mockConsoleError).toHaveBeenNthCalledWith(4, expect.stringContaining('\t- apiName'));
-      expect(mockConsoleError).toHaveBeenNthCalledWith(5, expect.stringContaining('\t- apiVersion'));
-
-      expect(exit).toHaveBeenCalledWith(INVALID_CONFIG_FORMAT);
-    };
-
-    it('should throw error if config and qualityGate is configured', () => {
-      const options: CalculateOptions = {
-        config: 'config',
-        qualityGate: 'quality-gate',
-      };
-
-      expectCombinationError(options);
-    });
-
-    it('should throw error if config and serviceName is configured', () => {
-      const options: CalculateOptions = {
-        config: 'config',
-        serviceName: 'test-service',
-      };
-
-      expectCombinationError(options);
-    });
-
-    it('should throw error if config and apiName is configured', () => {
-      const options: CalculateOptions = {
-        config: 'config',
-        apiName: 'test-api',
-      };
-
-      expectCombinationError(options);
-    });
-
-    it('should throw error if config and apiVersion is configured', () => {
-      const options: CalculateOptions = {
-        config: 'config',
-        apiVersion: 'test-version',
-      };
-
-      expectCombinationError(options);
-    });
-
-    it('resolves config from path', () => {
-      const options: CalculateOptions = {
-        config: 'config',
-      };
-
-      expect(calculate(getQualityGateApi(qualityGateApiMock), options)).resolves.toBeUndefined();
-
-      expect(resolveSnowWhiteConfig).toHaveBeenCalledWith('config');
-    });
-
-    const expectMissingConfigurationParameters = (options: CalculateOptions): void => {
-      expect(calculate(getQualityGateApi(qualityGateApiMock), options)).resolves.toBeUndefined();
-
-      expect(mockConsoleError).toHaveBeenNthCalledWith(
-        1,
-        expect.stringContaining('❌ Either define a config file or all of these calculation parameters:'),
-      );
-      expect(mockConsoleError).toHaveBeenNthCalledWith(2, expect.stringContaining('\t- qualityGate'));
-      expect(mockConsoleError).toHaveBeenNthCalledWith(3, expect.stringContaining('\t- serviceName'));
-      expect(mockConsoleError).toHaveBeenNthCalledWith(4, expect.stringContaining('\t- apiName'));
-      expect(mockConsoleError).toHaveBeenNthCalledWith(5, expect.stringContaining('\t- apiVersion'));
-
-      expect(exit).toHaveBeenCalledWith(INVALID_CONFIG_FORMAT);
-    };
-
-    it('should throw error if qualityGate is undefined in direct configuration', () => {
-      const options: CalculateOptions = {
-        qualityGate: 'quality-gate',
-      };
-
-      expectMissingConfigurationParameters(options);
-    });
-
-    it('should throw error if serviceName is undefined in direct configuration', () => {
-      const options: CalculateOptions = {
-        serviceName: 'test-service',
-      };
-
-      expectMissingConfigurationParameters(options);
-    });
-
-    it('should throw error if apiName is undefined in direct configuration', () => {
-      const options: CalculateOptions = {
-        apiName: 'test-api',
-      };
-
-      expectMissingConfigurationParameters(options);
-    });
-
-    it('should throw error if apiVersion is undefined in direct configuration', () => {
-      const options: CalculateOptions = {
-        apiVersion: 'test-version',
-      };
-
-      expectMissingConfigurationParameters(options);
     });
   });
 
