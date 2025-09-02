479773cbae1db962eb0aaaaa8a0ac7500a51d27e
Timon Borter <timon.borter@gmx.ch>
2025-09-02 16:47:39 +0200

    feat(api-gateway): improve test result card

--- microservices/api-gateway/src/main/webapp/app/entities/quality-gate/api-test-card.tsx
+++ microservices/api-gateway/src/main/webapp/app/entities/quality-gate/api-test-card.tsx
@@ -7,15 +7,68 @@
 import type { IApiTest } from 'app/shared/model/api-test.model';
 
 import ApiTestResultTable from 'app/entities/quality-gate/api-test-result-table';
-import React, { useState } from 'react';
+import { IApiTestResult } from 'app/shared/model/api-test-result.model';
+import React, { ReactElement, useMemo, useState } from 'react';
 import { Translate } from 'react-jhipster';
-import { Card, CardBody, CardTitle, Col, Collapse, Row, Table } from 'reactstrap';
+import { Badge, Card, CardBody, CardTitle, Col, Collapse, Progress, Row } from 'reactstrap';
 
 interface ApiTestCardProps {
   apiTest: IApiTest;
 }
 
+const renderStatusBadge = (containsTestResults: boolean, testResultsPassed: boolean): ReactElement => {
+  if (!containsTestResults) {
+    return (
+      <Badge color="danger">
+        <Translate contentKey="snowWhiteApp.reportStatus.IN_PROGRESS">In Progress</Translate>
+      </Badge>
+    );
+  } else if (testResultsPassed) {
+    return (
+      <Badge color="success">
+        <Translate contentKey="snowWhiteApp.reportStatus.PASSED">Passed</Translate>
+      </Badge>
+    );
+  }
+
+  return (
+    <Badge color="danger">
+      <Translate contentKey="snowWhiteApp.reportStatus.FAILED">Failed</Translate>
+    </Badge>
+  );
+};
+
+const renderStatusProgressBar = (containsTestResults: boolean, testResults: IApiTestResult[]): ReactElement => {
+  if (!containsTestResults) {
+    return <></>;
+  }
+
+  const total = testResults.length;
+  const failed = testResults.filter(r => !r.coverage || r.coverage < 1).length;
+  const passed = total - failed;
+
+  const passedPercentage = Math.round((passed / total) * 100);
+  const failedPercentage = Math.round((failed / total) * 100);
+
+  return (
+    <Progress multi>
+      <Progress bar color="success" value={passedPercentage}>
+        {passedPercentage}%
+      </Progress>
+      <Progress bar color="danger" value={failedPercentage}>
+        {failedPercentage}%
+      </Progress>
+    </Progress>
+  );
+};
+
 export const ApiTestCard: React.FC<ApiTestCardProps> = ({ apiTest }: ApiTestCardProps) => {
+  const containsTestResults = useMemo(() => (apiTest.testResults && apiTest.testResults.length > 0) || false, [apiTest.testResults]);
+  const apiTestResultStatus = useMemo(
+    () => apiTest.testResults?.some(apiTestResult => !apiTestResult.coverage || apiTestResult.coverage < 1) || false,
+    [apiTest.testResults],
+  );
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
@@ -23,14 +76,25 @@ export const ApiTestCard: React.FC<ApiTestCardProps> = ({ apiTest }: ApiTestCard
   return (
     <Card onClick={toggle}>
       <CardTitle>
-        <h4>
-          {apiTest.serviceName}: {apiTest.apiName} ({apiTest.apiVersion})
-        </h4>
+        <Row className="align-items-center">
+          <Col md={8}>
+            <h4>
+              {apiTest.serviceName}: <i>{apiTest.apiName}</i>{' '}
+              <small>
+                <Translate contentKey="snowWhiteApp.apiTest.apiVersion">Version</Translate>: {apiTest.apiVersion}
+              </small>
+            </h4>
+          </Col>
+          <Col md={2}>
+            <h4>{renderStatusBadge(containsTestResults, apiTestResultStatus)}</h4>
+          </Col>
+          <Col md={2}>{renderStatusProgressBar(containsTestResults, apiTest.testResults!)}</Col>
+        </Row>
       </CardTitle>
       <CardBody>
         <Collapse isOpen={isOpen}>
-          {apiTest.testResults && apiTest.testResults.length > 0 ? (
-            <ApiTestResultTable apiTestResults={apiTest.testResults} />
+          {containsTestResults ? (
+            <ApiTestResultTable apiTestResults={apiTest.testResults!} />
           ) : (
             <div className="alert alert-warning">
               <Translate contentKey="snowWhiteApp.apiTestResult.home.notFound">No API Test Results found</Translate>
