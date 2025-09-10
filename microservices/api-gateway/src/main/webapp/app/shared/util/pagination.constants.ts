18a7274220b7931c0bd65a81bd74eda7bfd4840b
Timon Borter <timon.borter@gmx.ch>
2025-09-10 18:48:51 +0200

    refactor(api-gateway): reduce default page size to 10

--- microservices/api-gateway/src/main/webapp/app/shared/util/pagination.constants.ts
+++ microservices/api-gateway/src/main/webapp/app/shared/util/pagination.constants.ts
@@ -4,7 +4,7 @@
  * See LICENSE file for full details.
  */
 
-export const ITEMS_PER_PAGE = 20;
+export const ITEMS_PER_PAGE = 10;
 export const ASC = 'asc';
 export const DESC = 'desc';
 export const SORT = 'sort';
