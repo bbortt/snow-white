d4bdef292043bcd67e6574e17700a5adad10186c
Timon Borter <timon.borter@gmx.ch>
2025-10-08 21:21:22 +0200

    refactor(api-sync-job): address memory issue concerns

---
+++ microservices/api-sync-job/src/main/java/io/github/bbortt/snow/white/microservices/api/sync/job/config/TaskExecutorConfig.java
@@ -0,0 +1,18 @@
+package io.github.bbortt.snow.white.microservices.api.sync.job.config;
+
+import java.util.concurrent.Executors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class TaskExecutorConfig {
+
+  @Bean
+  public AsyncTaskExecutor applicationTaskExecutor() {
+    return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+  }
+}
---
+++ microservices/api-sync-job/src/main/java/io/github/bbortt/snow/white/microservices/api/sync/job/service/impl/PagedBackstageEntitySpliterator.java
@@ -0,0 +1,66 @@
+package io.github.bbortt.snow.white.microservices.api.sync.job.service.impl;
+
+import io.github.bbortt.snow.white.microservices.api.sync.job.api.client.backstage.api.EntityApi;
+import io.github.bbortt.snow.white.microservices.api.sync.job.api.client.backstage.dto.Entity;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class PagedBackstageEntitySpliterator implements Spliterator<Entity> {
+
+  private final EntityApi backstageEntityApi;
+  private final int pageSize;
+  private int offset = 0;
+  private List<Entity> currentPage;
+  private int currentIndex = 0;
+
+  public PagedBackstageEntitySpliterator(
+    EntityApi backstageEntityApi,
+    int pageSize
+  ) {
+    this.backstageEntityApi = backstageEntityApi;
+    this.pageSize = pageSize;
+  }
+
+  @Override
+  public boolean tryAdvance(Consumer<? super Entity> action) {
+    if (currentPage == null || currentIndex >= currentPage.size()) {
+      currentPage = backstageEntityApi
+        .getEntitiesByQuery(
+          List.of("metadata.annotations", "spec.definition"),
+          pageSize,
+          offset,
+          null,
+          null,
+          null,
+          null,
+          null
+        )
+        .getItems();
+      offset += pageSize;
+      currentIndex = 0;
+    }
+
+    if (currentPage.isEmpty()) {
+      return false;
+    }
+
+    action.accept(currentPage.get(currentIndex++));
+    return true;
+  }
+
+  @Override
+  public Spliterator<Entity> trySplit() {
+    return null;
+  }
+
+  @Override
+  public long estimateSize() {
+    return Long.MAX_VALUE;
+  }
+
+  @Override
+  public int characteristics() {
+    return ORDERED | IMMUTABLE;
+  }
+}
