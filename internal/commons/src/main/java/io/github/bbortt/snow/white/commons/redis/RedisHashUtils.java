f741a594bc1e02b81a4a47f0eb8627de6166705b
Timon Borter <timon.borter@gmx.ch>
2025-10-27 11:03:00 +0100

    chore: update copyright notice

--- internal/commons/src/main/java/io/github/bbortt/snow/white/commons/redis/RedisHashUtils.java
+++ internal/commons/src/main/java/io/github/bbortt/snow/white/commons/redis/RedisHashUtils.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
 package io.github.bbortt.snow.white.commons.redis;
 
 import static io.micrometer.common.util.StringUtils.isEmpty;
