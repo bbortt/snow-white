76e3390685b396dd7c6be19c25cdba7e9911c02b
Timon Borter <timon.borter@gmx.ch>
2025-10-27 11:03:00 +0100

    chore: update copyright notice

--- microservices/kafka-event-filter/src/test/java/io/github/bbortt/snow/white/microservices/kafka/event/filter/api/kafka/serialization/ExportTraceServiceRequestSerdesTest.java
+++ microservices/kafka-event-filter/src/test/java/io/github/bbortt/snow/white/microservices/kafka/event/filter/api/kafka/serialization/ExportTraceServiceRequestSerdesTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
 package io.github.bbortt.snow.white.microservices.kafka.event.filter.api.kafka.serialization;
 
 import static org.assertj.core.api.Assertions.assertThat;
