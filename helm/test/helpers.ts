0bdfe0711b3183038b8c187e01ae85604b12fedf
Timon Borter <timon.borter@gmx.ch>
2025-11-09 15:08:44 +0100

    fix(helm): init postgresql databases for microservices

---
+++ helm/test/helpers.ts
@@ -0,0 +1,12 @@
+export const isSubset = (
+  subset: Record<string, string>,
+  superset: Record<string, any>,
+): boolean => {
+  for (const key in subset) {
+    if (!(key in superset) || subset[key] !== superset[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
