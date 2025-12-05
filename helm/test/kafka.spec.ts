fecf40813a9b53e96aba90d8354707dfaa667c53
Timon Borter <timon.borter@gmx.ch>
2025-12-05 16:37:48 +0100

    fix(helm): include confluentin kafka statefulset

--- helm/test/kafka.spec.ts
+++ helm/test/kafka.spec.ts
@@ -7,38 +7,23 @@
 import { describe, it, expect } from 'vitest';
 import { renderHelmChart } from './render-helm-chart';
 
-const getKafkaStatefulSet = (manifests: any[]) => {
+const getKafkaStatefulSet = async (manifests?: any[]) => {
+  if (!manifests) {
+    manifests = await renderHelmChart({
+      chartPath: 'charts/snow-white',
+    });
+  }
+
   const statefulSet = manifests.find(
     (m) =>
       m.kind === 'StatefulSet' &&
-      m.metadata.name.startsWith('test-release-kafka'),
+      m.metadata.name === 'snow-white-kafka-test-release',
   );
   expect(statefulSet).toBeDefined();
   return statefulSet;
 };
 
 describe('Kafka', () => {
-  it('is enabled by default', async () => {
-    const manifests = await renderHelmChart({
-      chartPath: 'charts/snow-white',
-    });
-
-    const statefulSet = getKafkaStatefulSet(manifests);
-
-    expect(statefulSet.spec.template.spec.containers[0].image).toMatch(
-      /^(registry-\d\.)?docker\.io\/bitnami\/kafka:.+$/,
-    );
-  });
-
-  it('is high-available by default', async () => {
-    const manifests = await renderHelmChart({
-      chartPath: 'charts/snow-white',
-    });
-
-    const statefulSet = getKafkaStatefulSet(manifests);
-    expect(statefulSet.spec.replicas).toBe(3);
-  });
-
   it('can be disabled via properties', async () => {
     const manifests = await renderHelmChart({
       chartPath: 'charts/snow-white',
@@ -54,5 +39,83 @@ describe('Kafka', () => {
     );
 
     expect(kafkaResources).toBeUndefined();
+  });
+
+  describe('StatefulSet', () => {
+    it('is enabled by default', async () => {
+      const statefulSet = await getKafkaStatefulSet();
+
+      expect(statefulSet.spec.template.spec.containers[0].image).toMatch(
+        /^(registry-\d\.)?docker\.io\/confluentinc\/cp-kafka:.+$/,
+      );
+    });
+
+    it('is high-available by default', async () => {
+      const statefulSet = await getKafkaStatefulSet();
+      expect(statefulSet.spec.replicas).toBe(3);
+    });
+
+    it('should have default labels', async () => {
+      const statefulSet = await getKafkaStatefulSet();
+
+      const { metadata } = statefulSet;
+      expect(metadata).toBeDefined();
+
+      expect(metadata.labels).toEqual({
+        'app.kubernetes.io/managed-by': 'Helm',
+        'app.kubernetes.io/version': 'test-version',
+        'helm.sh/chart': 'snow-white',
+        'app.kubernetes.io/instance': 'test-release',
+        'app.kubernetes.io/name': 'kafka',
+        'app.kubernetes.io/part-of': 'snow-white',
+      });
+    });
+
+    describe('replicas', () => {
+      it("should deploy three replica in 'high-available' mode", async () => {
+        const deployment = await getKafkaStatefulSet(
+          await renderHelmChart({
+            chartPath: 'charts/snow-white',
+            values: {
+              snowWhite: {
+                mode: 'high-available',
+              },
+            },
+          }),
+        );
+
+        expect(deployment.spec.replicas).toBe(3);
+      });
+
+      it("should deploy one replica in 'minimal' mode", async () => {
+        const deployment = await getKafkaStatefulSet(
+          await renderHelmChart({
+            chartPath: 'charts/snow-white',
+            values: {
+              snowWhite: {
+                mode: 'minimal',
+              },
+            },
+          }),
+        );
+
+        expect(deployment.spec.replicas).toBe(1);
+      });
+
+      it("should deploy three replica in 'auto-scaling' mode", async () => {
+        const deployment = await getKafkaStatefulSet(
+          await renderHelmChart({
+            chartPath: 'charts/snow-white',
+            values: {
+              snowWhite: {
+                mode: 'auto-scaling',
+              },
+            },
+          }),
+        );
+
+        expect(deployment.spec.replicas).toBe(3);
+      });
+    });
   });
 });
