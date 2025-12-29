f29dca0f56738fe987b3710b8013798860308d65
Timon Borter <timon.borter@gmx.ch>
2025-12-29 11:28:49 +0100

    docs(helm): deploymen instructions

--- microservices/openapi-coverage-stream/src/main/java/io/github/bbortt/snow/white/microservices/openapi/coverage/stream/Main.java
+++ microservices/openapi-coverage-stream/src/main/java/io/github/bbortt/snow/white/microservices/openapi/coverage/stream/Main.java
@@ -15,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
-  void main() {
-    run(Main.class);
+  public static void main(String[] args) {
+    run(Main.class, args);
   }
 }
