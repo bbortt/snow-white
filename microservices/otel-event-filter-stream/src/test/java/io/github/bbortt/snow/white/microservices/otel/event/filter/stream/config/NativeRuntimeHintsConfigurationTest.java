14a0e89e148ed01de216399954dade72e421514a
Timon Borter <timon.borter.1@postfinance.ch>
2026-02-25 18:47:48 +0100

    feat(helm): export metrics to infra collector

--- microservices/otel-event-filter-stream/src/test/java/io/github/bbortt/snow/white/microservices/otel/event/filter/stream/config/NativeRuntimeHintsConfigurationTest.java
+++ microservices/otel-event-filter-stream/src/test/java/io/github/bbortt/snow/white/microservices/otel/event/filter/stream/config/NativeRuntimeHintsConfigurationTest.java
@@ -116,7 +116,7 @@ class NativeRuntimeHintsConfigurationTest {
 
     otelRuntimeHints.registerHints(runtimeHintsMock, classLoaderMock);
 
-    verify(reflectionHintsMock, times(884)).registerType(
+    verify(reflectionHintsMock, times(1025)).registerType(
       any(Class.class),
       eq(INVOKE_PUBLIC_METHODS)
     );
