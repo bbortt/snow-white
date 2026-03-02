c2dc2b24ee011b57b71087ef1b9c6e0213efeacc
Timon Borter <timon.borter@gmx.ch>
2026-03-02 18:06:34 +0100

    chore: update copyright headers for 2026

--- .github/scripts/build-oci-images.sh
+++ .github/scripts/build-oci-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 #
-# Copyright (c) 2025 Timon Borter <timon.borter@gmx.ch>
+# Copyright (c) 2026 Timon Borter <timon.borter@gmx.ch>
 # Licensed under the Polyform Small Business License 1.0.0
 # See LICENSE file for full details.
 #
