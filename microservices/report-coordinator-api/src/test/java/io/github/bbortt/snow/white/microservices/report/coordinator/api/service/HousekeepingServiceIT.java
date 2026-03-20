87df06e6c664acf2014aa65405983f2b67cdc366
Timon Borter <timon.borter@gmx.ch>
2026-03-20 12:05:44 +0100

    fix(report-coordinator-api): time out only reports that are in initial status

--- microservices/report-coordinator-api/src/test/java/io/github/bbortt/snow/white/microservices/report/coordinator/api/service/HousekeepingServiceIT.java
+++ microservices/report-coordinator-api/src/test/java/io/github/bbortt/snow/white/microservices/report/coordinator/api/service/HousekeepingServiceIT.java
@@ -6,7 +6,10 @@
 
 package io.github.bbortt.snow.white.microservices.report.coordinator.api.service;
 
+import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.IN_PROGRESS;
+import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.NOT_STARTED;
 import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.TIMED_OUT;
+import static java.util.Arrays.stream;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -15,11 +18,16 @@ import io.github.bbortt.snow.white.microservices.report.coordinator.api.Abstract
 import io.github.bbortt.snow.white.microservices.report.coordinator.api.Main;
 import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.QualityGateReport;
 import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportParameter;
+import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus;
 import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.repository.QualityGateReportRepository;
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.parallel.Isolated;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -45,8 +53,18 @@ public class HousekeepingServiceIT extends AbstractReportCoordinationServiceIT {
   @Autowired
   private QualityGateReportRepository qualityGateReportRepository;
 
-  @Test
-  void postRequest_shouldTriggerHousekeeping() throws Exception {
+  public static Stream<ReportStatus> postRequest_shouldTriggerHousekeeping() {
+    return Stream.of(NOT_STARTED, IN_PROGRESS);
+  }
+
+  @BeforeEach
+  void beforeEachSetup() {
+    qualityGateReportRepository.deleteAll();
+  }
+
+  @MethodSource
+  @ParameterizedTest
+  void postRequest_shouldTriggerHousekeeping(ReportStatus reportStatus) {
     var qualityGateReport = qualityGateReportRepository.saveAndFlush(
       QualityGateReport.builder()
         .calculationId(CALCULATION_UUID)
@@ -54,9 +72,27 @@ public class HousekeepingServiceIT extends AbstractReportCoordinationServiceIT {
           ReportParameter.builder().calculationId(CALCULATION_UUID).build()
         )
         .qualityGateConfigName("qualityGateConfigName")
+        .reportStatus(reportStatus.getVal())
         .createdAt(Instant.now().minusSeconds(301L))
         .build()
     );
+
+    stream(ReportStatus.values())
+      .filter(s -> !List.of(NOT_STARTED, IN_PROGRESS, TIMED_OUT).contains(s))
+      .forEach(ignoredReportStatus -> {
+        var calculationId = UUID.randomUUID();
+        qualityGateReportRepository.saveAndFlush(
+          QualityGateReport.builder()
+            .calculationId(calculationId)
+            .reportParameter(
+              ReportParameter.builder().calculationId(calculationId).build()
+            )
+            .qualityGateConfigName("qualityGateConfigName")
+            .reportStatus(ignoredReportStatus.getVal())
+            .createdAt(Instant.now().minusSeconds(301L))
+            .build()
+        );
+      });
 
     await()
       .atMost(2, SECONDS)
@@ -71,5 +107,15 @@ public class HousekeepingServiceIT extends AbstractReportCoordinationServiceIT {
           .extracting(QualityGateReport::getReportStatus)
           .isEqualTo(TIMED_OUT)
       );
+
+    var allQualityGateReports = qualityGateReportRepository
+      .findAll()
+      .stream()
+      .filter(r -> !r.getCalculationId().equals(CALCULATION_UUID))
+      .toList();
+
+    assertThat(allQualityGateReports)
+      .isNotEmpty()
+      .allSatisfy(r -> assertThat(r.getReportStatus()).isNotEqualTo(TIMED_OUT));
   }
 }
