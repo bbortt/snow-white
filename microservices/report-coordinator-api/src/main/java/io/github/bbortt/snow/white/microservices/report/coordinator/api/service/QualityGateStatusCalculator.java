7e2688d748406bc6dcb25c785a02a5bb81473211
Timon Borter <timon.borter@gmx.ch>
2026-04-14 09:52:33 +0200

    fix(report-coordinator-api): report status updates on errornous results
    when multiple test results are being returned to `report-coordinator-api`
    (valid use case, when report includes multiple api specifications),
    the status calculation wasn't done right.
    e.g. when `SUCCESS` and `FAILURE` "arrived",
    the overall status was resetted to `IN_PROGRESS`,
    because the failure doesn't include test results.
    the `QualityGateStatusCalculator` has been adjusted accordingly.
    tests have been added.

--- microservices/report-coordinator-api/src/main/java/io/github/bbortt/snow/white/microservices/report/coordinator/api/service/QualityGateStatusCalculator.java
+++ microservices/report-coordinator-api/src/main/java/io/github/bbortt/snow/white/microservices/report/coordinator/api/service/QualityGateStatusCalculator.java
@@ -7,23 +7,37 @@
 package io.github.bbortt.snow.white.microservices.report.coordinator.api.service;
 
 import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.FAILED;
+import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.FINISHED_EXCEPTIONALLY;
 import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.IN_PROGRESS;
 import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.NOT_STARTED;
 import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.PASSED;
+import static io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus.TIMED_OUT;
 import static java.math.BigDecimal.ONE;
 import static org.springframework.util.CollectionUtils.isEmpty;
 
+import io.github.bbortt.snow.white.commons.testing.VisibleForTesting;
 import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ApiTestResult;
 import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.QualityGateReport;
+import io.github.bbortt.snow.white.microservices.report.coordinator.api.domain.model.ReportStatus;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 final class QualityGateStatusCalculator {
 
+  @VisibleForTesting
+  static final List<ReportStatus> TERMINAL_STATUS = List.of(
+    FAILED,
+    FINISHED_EXCEPTIONALLY,
+    TIMED_OUT
+  );
+
   QualityGateReport withUpdatedReportStatus(
     QualityGateReport qualityGateReport
   ) {
-    if (isEmpty(qualityGateReport.getApiTests())) {
+    if (TERMINAL_STATUS.contains(qualityGateReport.getReportStatus())) {
+      return qualityGateReport;
+    } else if (isEmpty(qualityGateReport.getApiTests())) {
       return qualityGateReport.withReportStatus(NOT_STARTED);
     } else if (
       qualityGateReport
