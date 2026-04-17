d31137a7ff861b3a04cd547bf7f9377f59dfe4d6
Timon Borter <timon.borter@gmx.ch>
2026-04-17 16:23:12 +0200

    feat(openapi-coverage-stream): add optional parameter coverage
    Each optional (non-required) parameter (in path, query) has been tested with valid values.
    This is a subset of `PARAMETER_COVERAGE`.

---
+++ microservices/openapi-coverage-stream/src/main/java/io/github/bbortt/snow/white/microservices/openapi/coverage/stream/service/calculator/OptionalParameterCoverageCalculator.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2026 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
+package io.github.bbortt.snow.white.microservices.openapi.coverage.stream.service.calculator;
+
+import static io.github.bbortt.snow.white.commons.quality.gate.OpenApiCriteria.OPTIONAL_PARAMETER_COVERAGE;
+import static java.lang.Boolean.TRUE;
+import static java.lang.String.format;
+import static java.lang.String.join;
+
+import io.github.bbortt.snow.white.commons.quality.gate.OpenApiCriteria;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import java.util.List;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+
+/**
+ * Calculator for the following criteria:
+ * Each optional (non-required) parameter (in path, query) has been tested with valid values.
+ * This is a subset of {@link OpenApiCriteria#PARAMETER_COVERAGE}.
+ *
+ * @see OpenApiCriteria#OPTIONAL_PARAMETER_COVERAGE
+ */
+@Slf4j
+@Component
+public class OptionalParameterCoverageCalculator
+  extends ParameterCoverageCalculator
+{
+
+  @Override
+  protected @NonNull OpenApiCriteria getSupportedOpenApiCriteria() {
+    return OPTIONAL_PARAMETER_COVERAGE;
+  }
+
+  @Override
+  protected List<Parameter> extractParameters(Operation operation) {
+    return operation
+      .getParameters()
+      .stream()
+      .filter(param -> !TRUE.equals(param.getRequired()))
+      .toList();
+  }
+
+  @Override
+  protected @Nullable String getAdditionalInformationOrNull(
+    @NonNull Set<String> uncoveredParameters
+  ) {
+    if (uncoveredParameters.isEmpty()) {
+      return null;
+    }
+
+    var sortedParameters = uncoveredParameters.stream().sorted().toList();
+
+    return format(
+      "The following optional parameters are uncovered: `%s`",
+      join("`, `", sortedParameters)
+    );
+  }
+}
