8973d6f1f7baab523c48313b304536df12495ed1
Timon Borter <timon.borter@gmx.ch>
2026-05-31 17:00:06 +0200

    chore: updated copyrights

--- microservices/quality-gate-api/src/test/java/io/github/bbortt/snow/white/microservices/quality/gate/api/service/DefaultOpenApiQualityGatesTest.java
+++ microservices/quality-gate-api/src/test/java/io/github/bbortt/snow/white/microservices/quality/gate/api/service/DefaultOpenApiQualityGatesTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
 package io.github.bbortt.snow.white.microservices.quality.gate.api.service;
 
 import static io.github.bbortt.snow.white.commons.quality.gate.OpenApiCoverageCriteria.PATH_COVERAGE;
