6f324455661f0fa21733fc5b9bdfed89691928be
Timon Borter <timon.borter@gmx.ch>
2026-05-31 17:00:06 +0200

    chore: updated copyrights

--- microservices/api-gateway/src/main/webapp/app/declarations.d.ts
+++ microservices/api-gateway/src/main/webapp/app/declarations.d.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
 declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
