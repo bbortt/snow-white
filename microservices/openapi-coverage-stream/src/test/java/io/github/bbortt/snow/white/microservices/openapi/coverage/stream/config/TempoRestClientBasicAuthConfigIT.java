0b796b451d0f39302c9f1627f515c07a43a6da4a
Timon Borter <timon.borter@gmx.ch>
2026-07-05 20:19:18 +0200

    chore(#1697): prepare tempo rest client in openapi-coverage-service

---
+++ microservices/openapi-coverage-stream/src/test/java/io/github/bbortt/snow/white/microservices/openapi/coverage/stream/config/TempoRestClientBasicAuthConfigIT.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2026 Timon Borter <timon.borter@gmx.ch>
+ * Licensed under the Polyform Small Business License 1.0.0
+ * See LICENSE file for full details.
+ */
+
+package io.github.bbortt.snow.white.microservices.openapi.coverage.stream.config;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import java.util.Base64;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestClient;
+import org.wiremock.spring.EnableWireMock;
+
+@EnableWireMock
+@SpringBootTest(
+  classes = TempoRestClientBasicAuthConfigIT.TestConfig.class,
+  properties = {
+    "tempo.url=${wiremock.server.baseUrl}",
+    "tempo.username=snow-white",
+    "tempo.password=s3cr3t",
+  }
+)
+class TempoRestClientBasicAuthConfigIT {
+
+  @EnableConfigurationProperties(TempoProperties.class)
+  @Import(TempoRestClientConfig.class)
+  static class TestConfig {}
+
+  @Autowired
+  @Qualifier("tempoRestClient")
+  private RestClient tempoRestClient;
+
+  @Nested
+  class TempoRestClientTest {
+
+    @Test
+    void shouldSendBasicAuthorizationHeader() {
+      assertThat(tempoRestClient).isNotNull();
+
+      stubFor(get(urlEqualTo("/api/search")).willReturn(ok()));
+
+      tempoRestClient.get().uri("/api/search").retrieve().toBodilessEntity();
+
+      var expectedCredentials = Base64.getEncoder().encodeToString(
+        "snow-white:s3cr3t".getBytes()
+      );
+
+      verify(
+        getRequestedFor(urlEqualTo("/api/search")).withHeader(
+          AUTHORIZATION,
+          equalTo("Basic " + expectedCredentials)
+        )
+      );
+    }
+  }
+}
