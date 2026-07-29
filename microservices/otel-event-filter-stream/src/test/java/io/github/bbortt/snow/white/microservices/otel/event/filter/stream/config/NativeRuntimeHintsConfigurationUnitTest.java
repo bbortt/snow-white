e4314c87f0f6ebd832f91220e53a0d13edbbae49
Timon Borter <timon.borter@gmx.ch>
2026-07-29 19:41:50 +0200

    fix(deps): update opentelemetry-proto to v1.11.0

--- microservices/otel-event-filter-stream/src/test/java/io/github/bbortt/snow/white/microservices/otel/event/filter/stream/config/NativeRuntimeHintsConfigurationUnitTest.java
+++ microservices/otel-event-filter-stream/src/test/java/io/github/bbortt/snow/white/microservices/otel/event/filter/stream/config/NativeRuntimeHintsConfigurationUnitTest.java
@@ -116,7 +116,7 @@ class NativeRuntimeHintsConfigurationUnitTest {
 
     otelRuntimeHints.registerHints(runtimeHintsMock, classLoaderMock);
 
-    verify(reflectionHintsMock, times(1014)).registerType(
+    verify(reflectionHintsMock, times(1017)).registerType(
       any(Class.class),
       eq(INVOKE_PUBLIC_METHODS)
     );
